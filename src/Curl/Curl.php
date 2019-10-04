9abc1241a865342cf87fd0a567351d03c15376ab
Riyan Satria <halo@riyansatria.tk>
2019-10-04 23:44:55 +0700

    Major updates

--- src/Curl/Curl.php
+++ src/Curl/Curl.php
@@ -3,36 +3,70 @@
 namespace BNI;
 
 class Curl {
-    public function init() {
-        global $ch;
-        $ch = curl_init();
+    static $method;
+    static $ch;
+    static $baseUrl;
+
+    public function bindArray($arrays, $prefix) {
+        foreach ($arrays as $a) {
+            foreach($a as $key => $value) {
+                $result[] = $key.$prefix.$value;
+            }
+        }
+        return $result;
+    }
+
+    public function get() {
+        self::$ch = curl_init();
+        self::$method = "GET";
         return new Curl;
     }
-    public function setUrl($url) {
-        global $ch;
-        curl_setopt($ch, CURLOPT_URL, $url);
+    public function post() {
+        self::$ch = curl_init();
+        self::$method = "POST";
         return new Curl;
     }
-    public function setHeader($param) {
-        global $ch;
-        curl_setopt($ch, CURLOPT_HTTPHEADER, $param);
+    public function url($url) {
+        self::$baseUrl = $url;
+        if(self::$method == "POST") {
+            curl_setopt(self::$ch, CURLOPT_URL, $url);
+        }
         return new Curl;
     }
-    public function setBody($param) {
-        global $ch;
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, $param);
+    public function parameter($params) {
+        $params = $this->bindArray($params, "=");
+        $p = "";
+        $i = 0;
+        foreach($params as $param) {
+            if($i++ == count($params) - 1) {
+                $p .= $param;
+            }else {
+                $p .= $param."&";
+            }
+        }
+        self::$baseUrl .= "?".$p;
+        curl_setopt(self::$ch, CURLOPT_URL, self::$baseUrl);
         return new Curl;
     }
-    public function setOpt($optName, $optValue) {
-        global $ch;
-        curl_setopt($ch, $optName, $optValue);
+    public function header($param) {
+        $params = $this->bindArray($param, ":");
+        curl_setopt(self::$ch, CURLOPT_HTTPHEADER, $params);
+        return new Curl;
+    }
+    public function body($param) {
+        if(self::$method == "POST") {
+            curl_setopt(self::$ch, CURLOPT_POST, 1);
+        }
+        curl_setopt(self::$ch, CURLOPT_POSTFIELDS, $param);
+        return new Curl;
+    }
+    public function options($optName, $optValue) {
+        curl_setopt(self::$ch, $optName, $optValue);
         return new Curl;
     }
     public function eksekusi() {
-        global $ch;
-        $res = curl_exec($ch);
-        curl_close($ch);
+        $res = curl_exec(self::$ch);
+        curl_close(self::$ch);
 		return $res;
     }
 }
